b629b575accbdbb3c323851bf9b4279e3ae37e6b
kongnayeon <seoultownroad@naver.com>
2022-07-30 02:08:47 +0900

    first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Nav.css";
+
+function Nav() {
+  const [show, setShow] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 50) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
+
+  return (
+    <nav className={`nav ${show && "nav__black"}`}>
+      <img
+        alt="Netflix logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+        className="nav__logo"
+        onClick={() => window.location.reload()}
+      />
+
+      <input
+        value={searchValue}
+        onChange={handleChange}
+        className="nav__input"
+        type="text"
+        placeholder="영화를 검색해주세요."
+      />
+      <img
+        alt="User logged"
+        src="https://v-phinf.pstatic.net/20210710_198/1625907512551lJ28B_JPEG/image.JPEG?type=w1000"
+        className="nav__avatar"
+      />
+    </nav>
+  );
+}
+
+export default Nav;
---
+++ src/pages/DetailPage/index.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "../../api/axios";
+
+export default function DetailPage() {
+  const { movieId } = useParams();
+  const [movies, setMovies] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(`/movie/${movieId}`);
+      setMovies(request.data);
+    }
+
+    fetchData();
+  }, [movieId]);
+
+  if (!movies) return <div>...loading</div>;
+
+  return (
+    <section>
+      <img
+        className="modal__poster-img"
+        src={`https://image.tmdb.org/t/p/original/${movies.backdrop_path}`}
+        alt="poster"
+      />
+    </section>
+  );
+}
